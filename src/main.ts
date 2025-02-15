ce0c33b795ba7f5d6b9482ef39494b1cdc882706
Michal <krzyzowskim1998@gmail.com>
2025-02-15 18:26:16 +0100

    FEAT: initialized store and creation of first reducer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
+import { counterReducer } from './app/store/counter.reducer';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideStore({
+      counter: counterReducer,
+    }),
+  ],
+});
