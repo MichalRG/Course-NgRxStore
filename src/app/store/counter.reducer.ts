1aa3b5394834ec87c7412778e0accff15ffc85f1
Michal <krzyzowskim1998@gmail.com>
2025-02-15 18:29:28 +0100

    DOCS: added legacy example how to create reducer function

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,3 +3,8 @@ import { createReducer } from '@ngrx/store';
 const initialState = 0;
 
 export const counterReducer = createReducer(initialState);
+
+// old way to create a reducer
+// export function counterReducerLegacyMethod(state = initialState) {
+//   return state;
+// }
