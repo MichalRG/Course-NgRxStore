e2ec66403088c011d03e6a9a7815990ebc5c4c66
Michal <krzyzowskim1998@gmail.com>
2025-02-15 19:36:40 +0100

    FEAT: installed and added to project effects schema

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideStore({
-      counter: counterReducer,
+        counter: counterReducer,
     }),
-  ],
+    provideEffects()
+],
 });
